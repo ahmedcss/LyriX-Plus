de76aecb1008dc290de318a0f98818e5238fe96d
ahmedHaddar <haddar.ahmed17@gmail.com>
2017-02-02 22:11:39 +0100

    Initial Commit

---
+++ Lyrix Plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lyrix Plus
+//
+//  Created by Ahmed Haddar on 02/02/2017.
+//  Copyright © 2017 Ahmed Haddar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
